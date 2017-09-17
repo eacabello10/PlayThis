04d40ec2baa2a7bc80678ece2b1a409415c28ca5
eacabello10 <ea.cabello10@uniandes.edu.co>
2017-09-16 21:18:53 -0500

    Lots of changes
    Principal: reading from json of mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/play-this-db';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	getInfo((info) => {
+		res.render('index', { title: 'Express',
+  		id: info[0].id,
+  		name: info[0].name,
+  		bio: info[0].biography
+		}
+		);
+	console.log(info);
+	});  
 });
+
+
+function getInfo(callback){
+	MongoClient.connect(url, function(err, db){
+  		var users = db.collection("usersCollection");
+  		users.find({id:123}).toArray((err2, users) =>{
+  		callback(users);
+  		});
+	});
+}
 
 module.exports = router;
