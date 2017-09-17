998df5f4aae496b0c5325e7727ec806b2365043e
JairoBm13 <j.bautista.m13@outlook.com>
2017-09-17 14:50:42 -0500

    arreglar error de parametros y uso de variable de ambiente para mongo

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var jsonwebtoken = require("jsonwebtoken");
 
-var url = "mongodb://localhost:27017/play-this-db";
-
+var url = process.env.MONGODB;
+console.log(url);
+console.log(process.env.MONGODB)
 var mongodb = require("mongodb");
-mongodb.connect(url, (err,db) => {
+mongodb.MongoClient.connect(process.env.MONGODB_URI, (err,db) => {
   if (err) console.error(err);
   console.log("success");
 });
