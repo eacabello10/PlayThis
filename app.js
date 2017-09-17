928372fd5990915ce4ac21afc02310f85ad17ca8
JairoBm13 <j.bautista.m13@outlook.com>
2017-09-17 13:29:48 -0500

    configurado para que le pegue al front en react

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'front/build')));
 
 app.use((req, res, next) => {
   if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === "JWT"){
