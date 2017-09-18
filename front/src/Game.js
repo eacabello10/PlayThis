4896c16c32d2375adfa1eb6a00edb5a806b0fa3a
Diana Catalina Solano Beltrán <dc.solano10@uniandes.edu.co>
2017-09-18 17:48:05 -0500

    Corregir presentacion juego

--- front/src/Game.js
+++ front/src/Game.js
@@ -7,16 +7,19 @@ import PropTypes from "prop-types";
 	}
 
 	render(){
+		//Podrian agregar esto en un formulario con un css de manera que se muestre mejor en la pagina
 		return(<div>
-			<div className="id">Game ID: {this.props.game.id}</div>
-			<div className="name">Name: {this.props.game.name}</div>
-			<div className="launchdate">Launch Date: {this.props.game.launchdate}</div>
+		       <form>
+			<div className="id" align="center">Game ID: {this.props.game.id}</div>
+			<div className="name" align="center">Name: {this.props.game.name}</div>
+			<div className="launchdate" align="center">Launch Date: {this.props.game.launchdate}</div>
 			{this.props.game.exclusive === true && <div className="exclusive">It's exclusive?: Yes</div>}
 			{this.props.game.exclusive === false && <div className="exclusive">It's exclusive?: No</div>}
 			<div className="genre">Genre: {this.props.game.genre}</div>
-			<div className="aproxDuration">Aprox. Duration: {this.props.game.aproxDuration}</div>
+			<div className="aproxDuration" align="center">Aprox. Duration: {this.props.game.aproxDuration}</div>
 			{this.props.game.multiplayer === true && <div className="multiplayer">Multiplayer: Yes</div>}
 			{this.props.game.multiplayer === false && <div className="multiplayer">Multiplayer: No</div>}
+		       </form>
 			</div>);
 			}
 }
